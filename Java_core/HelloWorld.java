7d042345ba37ba30fe6d054c195e753b4e94d747
Manoj-kumar-C <developermanoj2003@gmail.com>
2024-04-21 13:03:52 +0530

    some_updates

---
+++ Java_core/HelloWorld.java
@@ -0,0 +1,8 @@
+class HelloWorld{
+    
+    public static void main(String a[]){
+        System.out.println("Hello World");
+    }
+
+
+}
---
+++ Java_core/types.java
@@ -0,0 +1,12 @@
+// int, string, float, bool
+
+class types{
+    public static void main(String[] args){
+        final int a =10;
+        float b = 10.5f;
+        String c = "Hello"; 
+        boolean d =false;
+        char e = 'a';
+        System.out.println(a);
+    }
+}
